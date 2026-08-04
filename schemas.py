d3a8e5214fedbb836bfe9b80c28225cfbd0a43ea
shashwat7031 <shashwatjha384@gmail.com>
2026-08-05 01:08:31 +0530

    integrated the db and the main.py

--- schemas.py
+++ schemas.py
@@ -1,18 +1,35 @@
-from pydantic import BaseModel,ConfigDict,Field
+from datetime import datetime
+from pydantic import BaseModel,ConfigDict,Field,EmailStr
+
+class Userbase(BaseModel):
+    username: str = Field(min_length=1,max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(Userbase):
+    pass
+
+
+class UserResponse(Userbase):
+    model_config = ConfigDict(from_attributes=True)
+    id:int
+    image_file: str | None
+    image_path: str
 
 class Postbase(BaseModel):
     title:str = Field(min_length=1,max_length=100)
     content:str = Field(min_length=1)
-    author:str = Field(min_length=1,max_length=50)
-    id: int
-    date_posted:str
+
+
 class PostCreate(Postbase):
-    pass
+    user_id: int #temp
 
 class PostResponse(Postbase):
     model_config = ConfigDict(from_attributes=True)
 
 
     id:int
-    date_posted:str
+    user_id:int
+    date_posted:datetime
+    author: UserResponse
 
