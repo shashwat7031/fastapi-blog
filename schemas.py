a1ba1af792a1b17c9a07a9a1421b229410064231
shashwat7031 <shashwatjha384@gmail.com>
2026-08-07 12:19:26 +0530

    CRUD operations

--- schemas.py
+++ schemas.py
@@ -16,6 +16,12 @@ class UserResponse(Userbase):
     image_file: str | None
     image_path: str
 
+class UserUpdate(BaseModel):
+    username: str|None = Field(default=None,min_length=1,max_length=50)
+    email: EmailStr|None = Field(default=None,max_length=120)
+    image_file: str|None = Field(default=None,min_length=1,max_length=100)
+    
+
 class Postbase(BaseModel):
     title:str = Field(min_length=1,max_length=100)
     content:str = Field(min_length=1)
@@ -24,10 +30,12 @@ class Postbase(BaseModel):
 class PostCreate(Postbase):
     user_id: int #temp
 
+class PostUpdate(BaseModel):
+    title:str | None = Field(default=None,min_length=1,max_length=100)
+    content:str |None = Field(default = None,min_length=1)
+
 class PostResponse(Postbase):
     model_config = ConfigDict(from_attributes=True)
-
-
     id:int
     user_id:int
     date_posted:datetime
