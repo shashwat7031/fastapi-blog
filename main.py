ee137deec2c4f8ac8000c0a333f3d3a6ebde19cd
shashwat7031 <shashwatjha384@gmail.com>
2026-08-18 13:51:14 +0530

    made a functional blog site

--- main.py
+++ main.py
@@ -121,6 +121,14 @@ async def register_page(request: Request):
         {"title": "Register"},
     )
 
+@app.get("/account", include_in_schema=False,name="account_page")
+async def acc_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
+
 
 @app.exception_handler(StarletteHTTPException)
 async def general_http_exception_handler(
--- schemas.py
+++ schemas.py
@@ -36,7 +36,7 @@ class Postbase(BaseModel):
 
 
 class PostCreate(Postbase):
-    user_id: int #temp
+    pass
 
 class PostUpdate(BaseModel):
     title:str | None = Field(default=None,min_length=1,max_length=100)
